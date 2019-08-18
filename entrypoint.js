479087803ee04c034fbf52595df0104e19542812
Brian Surowiec <brian@13degrees.com>
2019-08-18 02:38:52 -0400

    Use console.log

--- entrypoint.js
+++ entrypoint.js
@@ -2,12 +2,13 @@ const { Toolkit } = require("actions-toolkit");
 const PullRequestChecker = require(".");
 const path = require("path");
 
-const { context, exit, github: {
-    pulls,
-    repos
-}, log } = new Toolkit({ secrets: ["GITHUB_TOKEN"] });
+const {
+    context,
+    exit,
+    github: { pulls, repos }
+} = new Toolkit({ secrets: ["GITHUB_TOKEN"] });
 
-const prChecker = new PullRequestChecker(context, pulls, repos, log);
+const prChecker = new PullRequestChecker(context, pulls, repos);
 
 const matchersPath = path.join(__dirname, ".github");
 console.log(`##[add-matcher]${path.join(matchersPath, "git-autosquash.json")}`);
--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 class PullRequestChecker {
-    constructor(context, pulls, repos, log) {
+    constructor(context, pulls, repos) {
         this.context = context;
         this.pulls = pulls;
         this.repos = repos;
-        this.log = log;
     }
 
     async go()
     {
         const commits = await this.pulls.listCommits(this.context.issue);
 
-        this.log(`Number of commits in the pull request: ${commits.data.length}`);
+        console.log(`Number of commits in the pull request: ${commits.data.length}`);
 
         let blockedCommits = 0;
         for (const commit of commits.data) {
             const isAutosquash = commit.commit.message.startsWith("fixup!") || commit.commit.message.startsWith("squash!");
 
             if (isAutosquash) {
-                this.log.info(`Commit ${commit.sha} is an autosquash commit: ${commit.url}`);
+                console.log(`Commit ${commit.sha} is an autosquash commit: ${commit.url}`);
 
                 blockedCommits++;
             }
         }
-        this.log(`Commits to block: ${blockedCommits}`)
+        console.log(`Commits to block: ${blockedCommits}`)
 
         return blockedCommits;
     }
